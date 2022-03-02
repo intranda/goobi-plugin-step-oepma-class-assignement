62f469153489772cc634da2a049dd548a38edeb8
Steffen Hankiewicz <steffen.hankiewicz@intranda.com>
2022-03-02 18:09:12 +0100

    first draft for replacements

--- plugin/src/main/java/de/intranda/goobi/plugins/OepmaClassAssignementStepPlugin.java
+++ plugin/src/main/java/de/intranda/goobi/plugins/OepmaClassAssignementStepPlugin.java
@@ -1,5 +1,8 @@
 package de.intranda.goobi.plugins;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * This file is part of a plugin for Goobi - a Workflow tool for the support of mass digitization.
  *
@@ -20,6 +23,10 @@ package de.intranda.goobi.plugins;
  */
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.configuration.SubnodeConfiguration;
 import org.goobi.beans.Step;
@@ -28,11 +35,25 @@ import org.goobi.production.enums.PluginReturnValue;
 import org.goobi.production.enums.PluginType;
 import org.goobi.production.enums.StepReturnValue;
 import org.goobi.production.plugin.interfaces.IStepPluginVersion2;
+import org.goobi.vocabulary.Field;
+import org.goobi.vocabulary.VocabRecord;
+import org.goobi.vocabulary.Vocabulary;
 
 import de.sub.goobi.config.ConfigPlugins;
+import de.sub.goobi.helper.exceptions.DAOException;
+import de.sub.goobi.helper.exceptions.SwapException;
+import de.sub.goobi.persistence.managers.VocabularyManager;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
 import net.xeoh.plugins.base.annotations.PluginImplementation;
+import ugh.dl.DocStruct;
+import ugh.dl.Fileformat;
+import ugh.dl.Metadata;
+import ugh.dl.Prefs;
+import ugh.exceptions.MetadataTypeNotAllowedException;
+import ugh.exceptions.PreferencesException;
+import ugh.exceptions.ReadException;
+import ugh.exceptions.WriteException;
 
 @PluginImplementation
 @Log4j2
@@ -42,10 +63,11 @@ public class OepmaClassAssignementStepPlugin implements IStepPluginVersion2 {
     private String title = "intranda_step_oepma_class_assignement";
     @Getter
     private Step step;
-    @Getter
-    private String value;
-    @Getter 
-    private boolean allowTaskFinishButtons;
+    private String vocabulary;
+    private String classField;
+    private String termsField;
+    private String metadataTitle;
+    private String metadataClass;
     private String returnPath;
 
     @Override
@@ -55,8 +77,12 @@ public class OepmaClassAssignementStepPlugin implements IStepPluginVersion2 {
                 
         // read parameters from correct block in configuration file
         SubnodeConfiguration myconfig = ConfigPlugins.getProjectAndStepConfig(title, step);
-        value = myconfig.getString("value", "default value"); 
-        allowTaskFinishButtons = myconfig.getBoolean("allowTaskFinishButtons", false);
+        vocabulary = myconfig.getString("vocabulary"); 
+        classField = myconfig.getString("class"); 
+        termsField = myconfig.getString("terms"); 
+        metadataTitle = myconfig.getString("metadataTitle"); 
+        metadataClass = myconfig.getString("metadataClass"); 
+        
         log.info("OepmaClassAssignement step plugin initialized");
     }
 
@@ -104,9 +130,82 @@ public class OepmaClassAssignementStepPlugin implements IStepPluginVersion2 {
     @Override
     public PluginReturnValue run() {
         boolean successful = true;
-        // your logic goes here
         
-        log.info("OepmaClassAssignement step plugin executed");
+        try {
+            // read mets file
+            Fileformat ff = step.getProzess().readMetadataFile();
+            Prefs prefs = step.getProzess().getRegelsatz().getPreferences();
+            DocStruct logical = ff.getDigitalDocument().getLogicalDocStruct();
+            DocStruct anchor = null;
+            if (logical.getType().isAnchor()) {
+                anchor = logical;
+                logical = logical.getAllChildren().get(0);
+            }
+            
+            // delete existing metadata of defined type
+            List<Metadata> originalMetadata = new ArrayList<>();
+            for (Metadata md : logical.getAllMetadata()) {
+                if (md.getType().getName().equals(metadataClass)) {
+                    originalMetadata.add(md);
+                } 
+            }
+            for (Metadata metadata : originalMetadata) {
+				logical.removeMetadata(metadata);
+			}
+            
+            // find out all classes that shall be assigned
+            String contentTitle = "";
+            for (Metadata md : logical.getAllMetadata()) {
+                if (md.getType().getName().equals(metadataTitle)) {
+                	contentTitle = md.getValue();
+                } 
+            }
+
+            Set <String> classesToAssign = new HashSet<String>();
+            
+            // load the vocabulary and all records
+            Vocabulary vocab = VocabularyManager.getVocabularyByTitle(vocabulary);
+            VocabularyManager.getAllRecords(vocab);
+            
+            // run through all records
+            for (VocabRecord myRecord : vocab.getRecords()) {
+            	List<Field> fields = myRecord.getFields();
+            	String myClass = null;
+            	String myTerms = null;
+            	
+            	// read class name and terms
+            	for (Field field : fields) {
+            		if (field.getLabel().equals(classField)) {
+            			myClass = field.getValue();
+            		}
+            		if (field.getLabel().equals(termsField)) {
+            			myTerms = field.getValue();
+            		}
+    			}
+            	
+            	// check if class matches
+            	String [] words = contentTitle.split(" ");
+            	for (String word : words) {
+					if (myTerms.contains(word)) {
+						classesToAssign.add(myClass);
+					}
+				}
+            }
+            
+            
+            //finally add all matching classes as new metadata
+            for (String s : classesToAssign) {
+            	Metadata md = new Metadata(prefs.getMetadataTypeByName(metadataClass));
+            	md.setValue(s);
+            	logical.addMetadata(md);            	
+			}
+            
+            // save the mets file
+            step.getProzess().writeMetadataFile(ff);
+        } catch (ReadException | PreferencesException | WriteException | IOException | InterruptedException | SwapException | DAOException | MetadataTypeNotAllowedException e) {
+            log.error(e);
+        }
+        
         if (!successful) {
             return PluginReturnValue.ERROR;
         }
